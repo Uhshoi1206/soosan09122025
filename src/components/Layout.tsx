80ad92025f6e280b44cd421d9f5ea93a928c7b0d
Uhshoi1206 <tu@c2.hu>
2025-12-10 22:17:20 +0700

    phone

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,25 +2,32 @@ import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import ScrollToTop from './ScrollToTop';
+import QuickContact from './QuickContact';
 import { CompareProvider } from '../contexts/CompareContextAstro';
+import { SiteSettingsProvider } from '../contexts/SiteSettingsContext';
 import { Toaster } from './ui/toaster';
 import './blog/BlogStyles.css';
+import type { SiteSettings } from '@/types/siteSettings';
 
 interface LayoutProps {
   children: React.ReactNode;
+  siteSettings?: Partial<SiteSettings>;
 }
 
-const Layout: React.FC<LayoutProps> = ({ children }) => {
+const Layout: React.FC<LayoutProps> = ({ children, siteSettings }) => {
   return (
-    <CompareProvider>
-      <div className="flex flex-col min-h-screen">
-        <Header />
-        <main className="flex-grow">{children}</main>
-        <Footer />
-        <ScrollToTop />
-        <Toaster />
-      </div>
-    </CompareProvider>
+    <SiteSettingsProvider settings={siteSettings}>
+      <CompareProvider>
+        <div className="flex flex-col min-h-screen">
+          <Header />
+          <main className="flex-grow">{children}</main>
+          <Footer />
+          <ScrollToTop />
+          <QuickContact />
+          <Toaster />
+        </div>
+      </CompareProvider>
+    </SiteSettingsProvider>
   );
 };
 
